81c68d8f47a95d6544b306daa66e4dd38e5cc684
Nariman Muldashev <nari11@pender.students.cs.ubc.ca>
2023-11-20 19:08:54 -0800

    modified getAnimals to get breed, added Projection, Join, GroupBy

--- appController.js
+++ appController.js
@@ -1,51 +1,93 @@
-const express = require('express');
-const appService = require('./appService');
+const express = require("express");
+const appService = require("./appService");
 
 const router = express.Router();
 
+const bodyParser = require("body-parser");
+
 // ----------------------------------------------------------
 
-router.get('/animals', async (req, res) => {
-    try {
-        const animals = await appService.getAnimals();
-        res.json({rows: animals});
-    } catch (err) {
-        console.log("error getting animal table");
-    }
+router.get("/animals", async (req, res) => {
+  try {
+    const animals = await appService.getAnimals();
+    res.json({ rows: animals });
+  } catch (err) {
+    console.log("error getting animal table");
+  }
 });
 
-router.get('/applications', async (req, res) => {
-    try {
-        const applications = await appService.getApplications();
-        res.json({rows: applications});
-    } catch (err) {
-        console.log("error getting animal table");
-    }
+router.get("/vaccinations", async (req, res) => {
+  try {
+    const animals = await appService.getVaccinationCounts();
+    res.json({ rows: animals });
+  } catch (err) {
+    console.log("error getting vaccinations count table");
+  }
+});
+
+router.get("/events", async (req, res) => {
+  try {
+    const events = await appService.getEvents();
+    res.json({ rows: events });
+  } catch (err) {
+    console.log("error getting events");
+  }
+});
+
+router.get("/applications", async (req, res) => {
+  try {
+    const applications = await appService.getApplications();
+    res.json({ rows: applications });
+  } catch (err) {
+    console.log("error getting animal table");
+  }
+});
+
+router.put("/projection", async (req, res) => {
+  try {
+    const { table_name, attributes } = req.body;
+    const table = await appService.getTable(table_name, attributes);
+    res.json(table);
+  } catch (err) {
+    console.log("error processing projection query:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 // Endpoint for INSERTING an entry in the Applies table
-// REQUIRES branchID, adopterID, animalID, applicationStatus and applicationDate in request body 
+// REQUIRES branchID, adopterID, animalID, applicationStatus and applicationDate in request body
 // EXPECTS applicationDate to be in the form "YYYY-MM-DD"
-router.post('/applications-submit', async (req, res) => {
-    const {branchID, adopterID, animalID, applicationStatus, applicationDate} = req.body;
-    const updateResult = await appService.submitApplication(branchID, adopterID, animalID, applicationStatus, applicationDate);
-    if (updateResult) {
-        res.json({ success: true });
-    } else {
-        res.status(500).json({ success: false });
-    }
+router.post("/applications-submit", async (req, res) => {
+  const { branchID, adopterID, animalID, applicationStatus, applicationDate } =
+    req.body;
+  const updateResult = await appService.submitApplication(
+    branchID,
+    adopterID,
+    animalID,
+    applicationStatus,
+    applicationDate
+  );
+  if (updateResult) {
+    res.json({ success: true });
+  } else {
+    res.status(500).json({ success: false });
+  }
 });
 
 // Endpoint for DELETING an entry in the Applies table
 // REQUIRES branchID, adopterID, animalID in request body to correctly identify which row to remove
-router.delete('/applications-withdraw', async (req, res) => {
-    const {branchID, adopterID, animalID} = req.body;
-    const updateResult = await appService.withdrawApplication(branchID, adopterID, animalID);
-    if (updateResult) {
-        res.json({ success: true });
-    } else {
-        res.status(500).json({ success: false });
-    }
+router.delete("/applications-withdraw", async (req, res) => {
+  const { branchID, adopterID, animalID } = req.body;
+  const updateResult = await appService.withdrawApplication(
+    branchID,
+    adopterID,
+    animalID
+  );
+  if (updateResult) {
+    res.json({ success: true });
+  } else {
+    res.status(500).json({ success: false });
+  }
 });
 
 // Endpoint for UPDATING an entry in the Applies table
@@ -53,75 +95,81 @@ router.delete('/applications-withdraw', async (req, res) => {
 //          the given applicationStatus and applicationDate are the NEW desired values for these fields
 // EXPECTS applicationDate to be in the form "YYYY-MM-DD"
 
-router.put('/applications-update', async (req, res) => {
-    const {branchID, adopterID, animalID, applicationStatus, applicationDate} = req.body;
-    const updateResult = await appService.updateApplication(branchID, adopterID, animalID, applicationStatus, applicationDate);
-    if (updateResult) {
-        res.json({ success: true });
-    } else {
-        res.status(500).json({ success: false });
-    }
+router.put("/applications-update", async (req, res) => {
+  const { branchID, adopterID, animalID, applicationStatus, applicationDate } =
+    req.body;
+  const updateResult = await appService.updateApplication(
+    branchID,
+    adopterID,
+    animalID,
+    applicationStatus,
+    applicationDate
+  );
+  if (updateResult) {
+    res.json({ success: true });
+  } else {
+    res.status(500).json({ success: false });
+  }
 });
 
 // API endpoints
 // Modify or extend these routes based on your project's needs.
-router.get('/check-db-connection', async (req, res) => {
-    const isConnect = await appService.testOracleConnection();
-    if (isConnect) {
-        res.send('connected');
-    } else {
-        res.send('unable to connect');
-    }
+router.get("/check-db-connection", async (req, res) => {
+  const isConnect = await appService.testOracleConnection();
+  if (isConnect) {
+    res.send("connected");
+  } else {
+    res.send("unable to connect");
+  }
 });
 
-router.get('/demotable', async (req, res) => {
-    const tableContent = await appService.fetchDemotableFromDb();
-    res.json({ data: tableContent });
+router.get("/demotable", async (req, res) => {
+  const tableContent = await appService.fetchDemotableFromDb();
+  res.json({ data: tableContent });
 });
 
 router.post("/initiate-demotable", async (req, res) => {
-    const initiateResult = await appService.initiateDemotable();
-    if (initiateResult) {
-        res.json({ success: true });
-    } else {
-        res.status(500).json({ success: false });
-    }
+  const initiateResult = await appService.initiateDemotable();
+  if (initiateResult) {
+    res.json({ success: true });
+  } else {
+    res.status(500).json({ success: false });
+  }
 });
 
 router.post("/insert-demotable", async (req, res) => {
-    const { id, name } = req.body;
-    const insertResult = await appService.insertDemotable(id, name);
-    if (insertResult) {
-        res.json({ success: true });
-    } else {
-        res.status(500).json({ success: false });
-    }
+  const { id, name } = req.body;
+  const insertResult = await appService.insertDemotable(id, name);
+  if (insertResult) {
+    res.json({ success: true });
+  } else {
+    res.status(500).json({ success: false });
+  }
 });
 
 router.post("/update-name-demotable", async (req, res) => {
-    const { oldName, newName } = req.body;
-    const updateResult = await appService.updateNameDemotable(oldName, newName);
-    if (updateResult) {
-        res.json({ success: true });
-    } else {
-        res.status(500).json({ success: false });
-    }
+  const { oldName, newName } = req.body;
+  const updateResult = await appService.updateNameDemotable(oldName, newName);
+  if (updateResult) {
+    res.json({ success: true });
+  } else {
+    res.status(500).json({ success: false });
+  }
 });
 
-router.get('/count-demotable', async (req, res) => {
-    const tableCount = await appService.countDemotable();
-    if (tableCount >= 0) {
-        res.json({
-            success: true,
-            count: tableCount
-        });
-    } else {
-        res.status(500).json({
-            success: false,
-            count: tableCount
-        });
-    }
+router.get("/count-demotable", async (req, res) => {
+  const tableCount = await appService.countDemotable();
+  if (tableCount >= 0) {
+    res.json({
+      success: true,
+      count: tableCount,
+    });
+  } else {
+    res.status(500).json({
+      success: false,
+      count: tableCount,
+    });
+  }
 });
-
 
 module.exports = router;
--- appService.js
+++ appService.js
@@ -1,190 +1,254 @@
-const oracledb = require('oracledb');
-const loadEnvFile = require('./utils/envUtil');
+const oracledb = require("oracledb");
+const loadEnvFile = require("./utils/envUtil");
 
-const envVariables = loadEnvFile('./.env');
+const envVariables = loadEnvFile("./.env");
 
 // Database configuration setup. Ensure your .env file has the required database credentials.
 const dbConfig = {
-    user: envVariables.ORACLE_USER,
-    password: envVariables.ORACLE_PASS,
-    connectString: `${envVariables.ORACLE_HOST}:${envVariables.ORACLE_PORT}/${envVariables.ORACLE_DBNAME}`
+  user: envVariables.ORACLE_USER,
+  password: envVariables.ORACLE_PASS,
+  connectString: `${envVariables.ORACLE_HOST}:${envVariables.ORACLE_PORT}/${envVariables.ORACLE_DBNAME}`,
 };
 
-
 async function getAnimals() {
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(
+      "SELECT * FROM AnimalInfo i, AnimalAdmits a WHERE i.breed = a.breed"
+    );
+    console.log(result);
+    return result.rows;
+  }).catch(() => {
+    return [];
+  });
+}
+
+// async function getAnimals() {
+//   return await withOracleDB(async (connection) => {
+//     const result = await connection.execute(
+//       "SELECT * FROM AnimalInfo i, AnimalAdmits a WHERE i.breed = a.breed"
+//     );
+//     console.log(result);
+//     return result.rows;
+//   }).catch(() => {
+//     return [];
+//   });
+// }
+
+async function getTable(table_name, attributes) {
+  try {
     return await withOracleDB(async (connection) => {
-        const result = await connection.execute('SELECT * FROM AnimalAdmits');
-        console.log(result);
-        return result.rows;
-    }).catch(() => {
-        return [];
+      const selectAttributes = attributes.join(", ");
+      const query = `SELECT ${selectAttributes} FROM ${table_name}`;
+      const result = await connection.execute(query);
+      return result.rows;
     });
+  } catch (error) {
+    console.error("Error executing SQL query:", error);
+    return [];
+  }
+}
+
+async function getVaccinationCounts() {
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(
+      "SELECT a.animalID, COUNT(v.animalID) AS vaccination_count FROM AnimalInfo i, AnimalAdmits a, Vaccination v WHERE i.breed = a.breed AND a.animalID = v.animalID GROUP BY a.animalID"
+    );
+    console.log(result);
+    return result.rows;
+  }).catch(() => {
+    return [];
+  });
+}
+
+async function getEvents() {
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(
+      "SELECT title, eventLocation, eventDate FROM Events"
+    );
+    console.log(result);
+    return result.rows;
+  }).catch(() => {
+    return [];
+  });
 }
 
 async function getApplications() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute('SELECT * FROM Applies');
-        console.log(result);
-        return result.rows;
-    }).catch(() => {
-        return [];
-    });
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute("SELECT * FROM Applies");
+    console.log(result);
+    return result.rows;
+  }).catch(() => {
+    return [];
+  });
 }
 
-async function submitApplication(branchID, adopterID, animalID, applicationStatus, applicationDate) {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(
-            `INSERT INTO Applies(branchID, adopterID, animalID, applicationStatus, applicationDate) 
+async function submitApplication(
+  branchID,
+  adopterID,
+  animalID,
+  applicationStatus,
+  applicationDate
+) {
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(
+      `INSERT INTO Applies(branchID, adopterID, animalID, applicationStatus, applicationDate) 
             VALUES(:branchID, :adopterID, :animalID, :applicationStatus, TO_DATE(:applicationDate, 'YYYY-MM-DD'))`,
-            { branchID, adopterID, animalID, applicationStatus, applicationDate },
-            { autoCommit: true }
-        );
-        return result.rowsAffected && result.rowsAffected > 0;
-    }).catch(() => {
-        return false;
-    });
+      { branchID, adopterID, animalID, applicationStatus, applicationDate },
+      { autoCommit: true }
+    );
+    return result.rowsAffected && result.rowsAffected > 0;
+  }).catch(() => {
+    return false;
+  });
 }
 
 async function withdrawApplication(branchID, adopterID, animalID) {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(
-            `DELETE FROM Applies 
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(
+      `DELETE FROM Applies 
              WHERE branchID = :branchID AND adopterID = :adopterID AND animalID = :animalID`,
-            { branchID, adopterID, animalID },
-            { autoCommit: true }
-        );
-        return result.rowsAffected && result.rowsAffected > 0;
-    }).catch(() => {
-        return false;
-    });
+      { branchID, adopterID, animalID },
+      { autoCommit: true }
+    );
+    return result.rowsAffected && result.rowsAffected > 0;
+  }).catch(() => {
+    return false;
+  });
 }
 
-async function updateApplication(branchID, adopterID, animalID, applicationStatus, applicationDate) {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(
-            `UPDATE Applies 
+async function updateApplication(
+  branchID,
+  adopterID,
+  animalID,
+  applicationStatus,
+  applicationDate
+) {
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(
+      `UPDATE Applies 
              SET applicationStatus = :applicationStatus,
                  applicationDate = TO_DATE(:applicationDate, 'YYYY-MM-DD')
              WHERE branchID = :branchID AND adopterID = :adopterID AND animalID = :animalID`,
-            { branchID, adopterID, animalID, applicationStatus, applicationDate },
-            { autoCommit: true }
-        );
-        return result.rowsAffected && result.rowsAffected > 0;
-    }).catch(() => {
-        return false;
-    });
+      { branchID, adopterID, animalID, applicationStatus, applicationDate },
+      { autoCommit: true }
+    );
+    return result.rowsAffected && result.rowsAffected > 0;
+  }).catch(() => {
+    return false;
+  });
 }
-
 
 // ----------------------------------------------------------
 // Wrapper to manage OracleDB actions, simplifying connection handling.
 async function withOracleDB(action) {
-    let connection;
-    try {
-        connection = await oracledb.getConnection(dbConfig);
-        return await action(connection);
-    } catch (err) {
+  let connection;
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+    return await action(connection);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
         console.error(err);
-        throw err;
-    } finally {
-        if (connection) {
-            try {
-                await connection.close();
-            } catch (err) {
-                console.error(err);
-            }
-        }
+      }
     }
+  }
 }
-
 
 // ----------------------------------------------------------
 // Core functions for database operations
 // Modify these functions, especially the SQL queries, based on your project's requirements and design.
 async function testOracleConnection() {
-    return await withOracleDB(async (connection) => {
-        return true;
-    }).catch(() => {
-        return false;
-    });
+  return await withOracleDB(async (connection) => {
+    return true;
+  }).catch(() => {
+    return false;
+  });
 }
 
 async function fetchDemotableFromDb() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute('SELECT * FROM DEMOTABLE');
-        return result.rows;
-    }).catch(() => {
-        return [];
-    });
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute("SELECT * FROM DEMOTABLE");
+    return result.rows;
+  }).catch(() => {
+    return [];
+  });
 }
 
 async function initiateDemotable() {
-    return await withOracleDB(async (connection) => {
-        try {
-            await connection.execute(`DROP TABLE DEMOTABLE`);
-        } catch(err) {
-            console.log('Table might not exist, proceeding to create...');
-        }
+  return await withOracleDB(async (connection) => {
+    try {
+      await connection.execute(`DROP TABLE DEMOTABLE`);
+    } catch (err) {
+      console.log("Table might not exist, proceeding to create...");
+    }
 
-        const result = await connection.execute(`
+    const result = await connection.execute(`
             CREATE TABLE DEMOTABLE (
                 id NUMBER PRIMARY KEY,
                 name VARCHAR2(20)
             )
         `);
-        return true;
-    }).catch(() => {
-        return false;
-    });
+    return true;
+  }).catch(() => {
+    return false;
+  });
 }
 
 async function insertDemotable(id, name) {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(
-            `INSERT INTO DEMOTABLE (id, name) VALUES (:id, :name)`,
-            [id, name],
-            { autoCommit: true }
-        );
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(
+      `INSERT INTO DEMOTABLE (id, name) VALUES (:id, :name)`,
+      [id, name],
+      { autoCommit: true }
+    );
 
-        return result.rowsAffected && result.rowsAffected > 0;
-    }).catch(() => {
-        return false;
-    });
+    return result.rowsAffected && result.rowsAffected > 0;
+  }).catch(() => {
+    return false;
+  });
 }
 
 async function updateNameDemotable(oldName, newName) {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute(
-            `UPDATE DEMOTABLE SET name=:newName where name=:oldName`,
-            [newName, oldName],
-            { autoCommit: true }
-        );
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute(
+      `UPDATE DEMOTABLE SET name=:newName where name=:oldName`,
+      [newName, oldName],
+      { autoCommit: true }
+    );
 
-        return result.rowsAffected && result.rowsAffected > 0;
-    }).catch(() => {
-        return false;
-    });
+    return result.rowsAffected && result.rowsAffected > 0;
+  }).catch(() => {
+    return false;
+  });
 }
 
 async function countDemotable() {
-    return await withOracleDB(async (connection) => {
-        const result = await connection.execute('SELECT Count(*) FROM DEMOTABLE');
-        return result.rows[0][0];
-    }).catch(() => {
-        return -1;
-    });
+  return await withOracleDB(async (connection) => {
+    const result = await connection.execute("SELECT Count(*) FROM DEMOTABLE");
+    return result.rows[0][0];
+  }).catch(() => {
+    return -1;
+  });
 }
 
 module.exports = {
-    testOracleConnection,
-    fetchDemotableFromDb,
-    initiateDemotable, 
-    insertDemotable, 
-    updateNameDemotable, 
-    countDemotable,
-    getAnimals,
-    getApplications,
-    submitApplication,
-    withdrawApplication,
-    updateApplication
+  testOracleConnection,
+  fetchDemotableFromDb,
+  initiateDemotable,
+  insertDemotable,
+  updateNameDemotable,
+  countDemotable,
+  getEvents,
+  getAnimals,
+  getTable,
+  getVaccinationCounts,
+  getApplications,
+  submitApplication,
+  withdrawApplication,
+  updateApplication,
 };
